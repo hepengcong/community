9f565de66da86b992384380c2779e50a98814ac1
hepengcong <helium3@sina.com>
2020-04-30 17:40:08 +0800

    完成持久化登录

--- src/main/java/com/practice/community/community/Mapper/UserMapper.java
+++ src/main/java/com/practice/community/community/Mapper/UserMapper.java
@@ -3,7 +3,11 @@ package com.practice.community.community.Mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.practice.community.community.model.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
